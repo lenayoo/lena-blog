f4513d4d1367addd3ba94000e90f7de3aecf8216
Lena Yoo <lenayoo17@gmail.com>
2024-10-12 19:52:13 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <header>
+        <div>Lena BLOG</div>
+        <div>posts</div>
+        <div>contact</div>
+      </header>
+      <main></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
