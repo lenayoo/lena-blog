9ca8941d22f401bf5946dab292081c872e1befb7
Eunmi Yoo <lenayoo@Eunmis-MacBook-Pro.local>
2024-10-22 23:51:44 +0900

    few fix for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import './style.css';
 
 export default function Header() {
   return (
-    <header className='header'>
-      <div className='header_logo'>Lena BLOG</div>
-      <div className='header-menu'>
-        <div className='header-write'>write</div>
-        <div className='header-posts'>posts</div>
-        <div className='header-contact'>contact</div>
+    <header className='top'>
+      <div className='top-logo'>Lena BLOG</div>
+      <div className='top-menu'>
+        <div className='top-write'>write</div>
+        <div className='top-posts'>posts</div>
+        <div className='top-contact'>contact</div>
       </div>
     </header>
   );
