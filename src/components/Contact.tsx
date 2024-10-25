357464ce8925eaa214cae242c5d5daa1c90dfb1a
lenayoo <lenayoo17@gmail.com>
2024-10-26 00:04:04 +0900

    add new layout

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <>
+      <h2>
+        늘 무엇인가를 새로 만들고, 부수고, 또 무엇인가를 이뤄내야 된다고
+        생각하지만 그럼에도 불고하고, 늘 만드는 사람
+      </h2>
+      <p>그런 만드는 사람에 if you want to reach out to me</p>
+      <p>Email: lenayoo17@gmail.com</p>
+    </>
+  );
+}
